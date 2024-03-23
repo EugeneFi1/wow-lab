928ab1457bf5aeb719e35fdd1b743d80f493d1d6
EugeneFi1 <eugenefil.ua@gmail.com>
2024-03-23 13:53:28 +0200

    fix default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import {Routes} from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '', redirectTo: '/projects', pathMatch: 'full'
+    },
+    {
         path: "projects",
         loadChildren: () => import("./modules/projects/projects.module").then(m => m.ProjectsModule)
     }
